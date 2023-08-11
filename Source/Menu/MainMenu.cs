fb7ff196a39cff4643b109f28e90df34d303974d
erik-lance <eriklance@gmail.com>
2023-08-11 20:57:37 +0800

    Added Initial Main Menu

---
+++ Source/Menu/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TactiGames.Source.Menu
+{
+    public class MainMenu
+    {
+        private SpriteBatch _spriteBatch;
+
+        public MainMenu(SpriteBatch spriteBatch)
+        {
+            _spriteBatch = spriteBatch;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}

c7f662a31c4e85d806b1ff9e06005b50cb28c3ba
erik-lance <eriklance@gmail.com>
2023-08-15 19:52:53 +0800

    Added initial UI button element

---
+++ Source/UI/Button.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TactiGames.Source.UI
+{
+    public class Button
+    {
+        public Rectangle Bounds { get; private set; }
+        public string Text { get; private set; }
+
+        public Button(Rectangle bounds, string text)
+        {
+            Bounds = bounds;
+            Text = text;
+        }
+
+        public bool IsClicked(Point clickPosition)
+        {
+            return Bounds.Contains(clickPosition);
+        }
+    }
+}
